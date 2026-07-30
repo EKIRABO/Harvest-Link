8ce918cb11b51883beeec73cc5495160730ef796
EKIRABO <e.kirabo@alustudent.com>
2026-07-30 16:32:48 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,3 @@
-
-
 require("dotenv").config();
 const mysql = require("mysql2/promise");
 
@@ -9,6 +7,11 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
